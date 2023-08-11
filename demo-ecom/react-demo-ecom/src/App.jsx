63eef53bef45ef70de1cddf1acbff1647833e0ab
MahendraSH <maahi2001sh@gmail.com>
2023-08-11 13:11:36 +0530

    demo-ecom

---
+++ demo-ecom/react-demo-ecom/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      <button className='btn btn-primary'>some</button>
+    </div>
+  )
+}
+
+export default App
