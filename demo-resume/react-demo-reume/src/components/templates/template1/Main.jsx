fa60856c5f71006a21fe1beb1b557a1accf0b5d4
Mahendra  S H <88196283+MahendraSH@users.noreply.github.com>
2023-08-11 20:33:52 +0530

    made responsive

--- demo-resume/react-demo-reume/src/components/templates/template1/Main.jsx
+++ demo-resume/react-demo-reume/src/components/templates/template1/Main.jsx
@@ -13,28 +13,27 @@ const Main = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-zinc-600">
           <div className="  ">
-            <div className="card md:card-side bg-gradient-to-tr from-slate-300 via-fuchsia-50 to-cyan-100 shadow-xl w-full  p-5">
-              <figure className="w-96">
+            <div className="card md:card-side bg-gradient-to-tr from-slate-300 via-fuchsia-50 to-cyan-100shadow-xl w-full  p-5">
+            <figure className="w-56 lg:w-96">
+              {" "}
+              <img
+                className="w-56 mask mask-circle hover:mask-squircle"
+                // src="https://res.cloudinary.com/dranaclni/image/upload/v1686133460/cenIN/avatars/cf6k0nkbrqnldjok8r46.png"
+                src="https://avatars.githubusercontent.com/u/88196283?v=4"
+              />
+              <div className=" hidden lg: card-body lg:!flex"></div>
+              <div className=" hidden lg: card-body lg:!flex "></div>
+            </figure>
+            <div className="card-body w-60 md:w-96">
+              <h2 className="card-title">👋 I am Mahendra S H</h2>
+              <p className=" font-normal">
                 {" "}
-                <img
-                  className="w-56 mask mask-circle hover:mask-squircle"
-                  // src="https://res.cloudinary.com/dranaclni/image/upload/v1686133460/cenIN/avatars/cf6k0nkbrqnldjok8r46.png"
-                  src="https://avatars.githubusercontent.com/u/88196283?v=4"
-                />
-                <div className=" hidden lg: card-body lg:!flex"></div>
-                <div className=" hidden lg: card-body lg:!flex "></div>
-              </figure>
-              <div className="card-body w-80 lg:w-96">
-                <h2 className="card-title">👋 I am Mahendra S H</h2>
-                <p className=" font-normal">
-                  {" "}
-                  "Computer science student and Passionate about creating
-                  responsive and user-friendly web applications"
-                </p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary btn-sm"> contact</button>
-                  <button className="btn btn-ghost btn-sm"> download cv</button>
-                </div>
+                "Computer science student and Passionate about creating
+                responsive and user-friendly web applications"
+              </p>
+              <div className="card-actions justify-center md:justify-end">
+                <button className="btn btn-primary btn-sm"> contact</button>
+                <button className="btn btn-ghost btn-sm"> download cv</button>
               </div>
             </div>
           </div>
