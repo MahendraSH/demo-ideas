73551f1d9616bbf4b49c32730723dfb8d5704676
MahendraSH <maahi2001sh@gmail.com>
2023-08-11 12:13:23 +0530

    demo-resune idea

---
+++ demo-resume/react-demo-reume/src/components/templates/template1/Main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <div
+        className="hero min-h-screen xl:w-ful  lg:w-full md: w-full sm:w-80 " id="main"
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-zinc-600">
+          <div className="  ">
+            <div className="card md:card-side bg-gradient-to-tr from-slate-300 via-fuchsia-50 to-cyan-100 shadow-xl w-full  p-5">
+              <figure className="w-96">
+                {" "}
+                <img
+                  className="w-56 mask mask-circle hover:mask-squircle"
+                  // src="https://res.cloudinary.com/dranaclni/image/upload/v1686133460/cenIN/avatars/cf6k0nkbrqnldjok8r46.png"
+                  src="https://avatars.githubusercontent.com/u/88196283?v=4"
+                />
+                <div className=" hidden lg: card-body lg:!flex"></div>
+                <div className=" hidden lg: card-body lg:!flex "></div>
+              </figure>
+              <div className="card-body w-80 lg:w-96">
+                <h2 className="card-title">👋 I am Mahendra S H</h2>
+                <p className=" font-normal">
+                  {" "}
+                  "Computer science student and Passionate about creating
+                  responsive and user-friendly web applications"
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary btn-sm"> contact</button>
+                  <button className="btn btn-ghost btn-sm"> download cv</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
--- demo-resume/react-demo-reume/src/pages/Porfolio.jsx
+++ demo-resume/react-demo-reume/src/pages/Porfolio.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import Nabar1 from '../components/templates/template1/Nabar1'
-import Nabar2 from '../components/templates/template1/Nabar2'
-import Nabar3 from '../components/templates/template1/Nabar3'
-import Nabar4 from '../components/templates/template1/Nabar4'
-import Nabar5 from '../components/templates/template1/Nabar5'
+import React from "react";
+
+import Nabar5 from "../components/templates/template1/Navbar";
 
 const Porfolio = () => {
   return (
     <div>
-        <Nabar5/>
-      
+      <Nabar5 />
     </div>
-  )
-}
+  );
+};
 
-export default Porfolio
+export default Porfolio;
