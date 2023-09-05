a8d9f81840a33a6289646d87b04ad7c8671f40fb
MahendraSH <maahi2001sh@gmail.com>
2023-09-05 14:41:59 +0530

    create: demo-resume-website-builder-nextjs  demo-resume-website-builder-react  demo-resume-website-builder-node

---
+++ demo-resume-website-builder/demo-resume-website-builder-nextjs/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1 className=' justify-center'>demo-resume-website-builder-nextjs </h1>
+    </div>
+  )
+}
+
+export default MainPage
