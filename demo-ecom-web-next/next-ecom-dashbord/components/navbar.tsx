46efaba7dbd6cc101a584a2a08d11fd820f7a916
MahendraSH <maahi2001sh@gmail.com>
2023-09-08 15:40:34 +0530

    created: combobox bor seclecting and createing store

--- demo-ecom-web-next/next-ecom-dashbord/components/navbar.tsx
+++ demo-ecom-web-next/next-ecom-dashbord/components/navbar.tsx
@@ -1,14 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import { FC } from "react";
 import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switecher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = ({}) => {
+const Navbar: FC<NavbarProps> = async ({}) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const items = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className=" shadow-md shadow-zinc-600 ">
       <div className=" flex h-14 items-center px-4 ">
-        <div>this is a store sector</div>
+        <StoreSwitcher items={items} />
         <MainNav className="mx-6" />
         <div className=" ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
